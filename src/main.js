8d633453c1a96a26d61ba41b799fdd2709058a2c
Harshita Mathur <harshitamathur17121@gmail.com>
2023-07-26 12:27:09 +0530

    Updated file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
- // Sticky Navbar
- window.addEventListener('scroll', function() {
-  let header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
-});
+
+    // Get the navbar element
+    const navbar = document.getElementById('navbar');
+
+    // Function to handle the scroll event
+    function handleScroll() {
+        if (window.scrollY > 100) {
+            // Add a class to change the background color when scrolling
+            navbar.classList.add('bg-white');
+        } else {
+            // Remove the class when the user scrolls back to the top
+            navbar.classList.remove('bg-white');
+        }
+    }
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
